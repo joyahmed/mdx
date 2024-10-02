92fbb5d4103c0ee4032f19e7544a31d9b5f3cbe4
JOY <joy_ahmed_007@yahoo.com>
2024-10-02 23:10:06 +0600

    ✅Styled Home

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,21 @@ import React from 'react';
 const Home = async () => {
 	const posts = await getPosts();
 	return (
-		<div>
-			<div className='flex flex-col gap-4'>
-				<h2 className='text-lg font-semibold'>Blog Posts</h2>
-				<ul className='list-disc pl-5'>
-					{posts.map((post, index) => (
-						<li key={index}>
-							<Link
-								href={`/blog/${post.title
-									.toLowerCase()
-									.replace(/\s+/g, '-')}`}
-							>
-								{post.title}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</div>
+		<div className='flex flex-col items-center justify-center h-screen w-screen'>
+			<h2 className='font-bold text-4xl'>Blog Posts</h2>
+			<ul className='list-disc pl-5 font-semibold'>
+				{posts.map((post, index) => (
+					<li key={index}>
+						<Link
+							href={`/blog/${post.title
+								.toLowerCase()
+								.replace(/\s+/g, '-')}`}
+						>
+							{post.title}
+						</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
