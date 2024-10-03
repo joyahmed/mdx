47b635a87fb9487e36b9433297f7b1837917bc01
JOY <joy_ahmed_007@yahoo.com>
2024-10-03 09:22:00 +0600

    ✅Typography

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,23 +1,28 @@
 // app/blog/[slug]/page.tsx
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
 import Markdown from '@/components/Markdown';
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
 
 interface Params {
-  slug: string;
+	slug: string;
 }
 
 export default async function Post({ params }: { params: Params }) {
-  const { slug } = params;
-  const filePath = path.join(process.cwd(), 'components', 'posts', `${slug}.mdx`);
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const { data, content } = matter(fileContents);
+	const { slug } = params;
+	const filePath = path.join(
+		process.cwd(),
+		'components',
+		'posts',
+		`${slug}.mdx`
+	);
+	const fileContents = fs.readFileSync(filePath, 'utf8');
+	const { data, content } = matter(fileContents);
 
-  return (
-    <article className="prose mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
-      <Markdown content={content} /> {/* Use the Markdown component */}
-    </article>
-  );
+	return (
+		<article className='prose mx-auto p-4'>
+			<h1 className='text-4xl font-bold mb-4'>{data.title}</h1>
+			<Markdown content={content} />
+		</article>
+	);
 }
